78aec3f637a15aaf45deb3c3bcf8d06955c77cf4
R4nd0mJ0k3r <lewishides@gmail.com>
2017-04-20 11:38:43 +0100

    Changed hidden files show/hide commands

--- General.sh
+++ General.sh
@@ -8,8 +8,8 @@ alias alias-reload='source ~/.bash_profile'
 alias root-go='cd //'
 
 #Show/Hide hidden files & folders
-alias hiddenfiles-show='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
-alias hiddenfiles-hide='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
+alias hiddenfiles-show='defaults write com.apple.finder AppleShowAllFiles true; killall Finder'
+alias hiddenfiles-hide='defaults write com.apple.finder AppleShowAllFiles false; killall Finder'
 
 #Destroy duplicate entries in the "open with" menu
 alias clean-openwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user;killall Finder; echo "Rebuilt Open With, relaunching Finder"'
